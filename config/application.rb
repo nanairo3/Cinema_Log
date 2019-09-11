b061a5f69cafda918d34fa00706d0f0592a8c38a
nanairo3 <nao23333@gmail.com>
2019-09-11 21:44:08 +0900

    Timezone周りインデント修正

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,13 @@ module Environment
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
     config.i18n.default_locale = :ja
-    
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    
+
     config.generators do |g|
       g.template_engine :slim
       g.javascripts     false
