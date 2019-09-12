2538d73a1a4d903d704cb7d137633d898d6d8bd3
nanairo3 <nao23333@gmail.com>
2019-09-12 22:59:42 +0900

    routeing, sisseonController修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root 'homes#index'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
