3baa5e6116a436cdb67221f95c31b71511fe604f
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-09-13 02:28:35 +0000

    users modelテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'userA' }
+    sequence(:email) { |n| "a#{n}@example.com" }
+    password { 'apassword' }
+    password_confirmation { 'apassword' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '成功' do
+    it 'name, email,passwordのとき成功' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
+  
+   describe '失敗' do
+    it 'emailがnilのとき無効' do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include('を入力してください')
+    end
+  
+    it 'nameがnilのとき無効' do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include('を入力してください')
+    end
+  
+    it 'passwordがnilのとき無効' do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include('を入力してください')
+    end
+    
+    it 'nameが20文字以上の時無効' do
+      user = build(:user, name: "a"*21)
+      user.valid?
+      expect(user.errors[:name]).to include('は20文字以内で入力してください')
+    end
+  
+    it 'passwordが6文字以下で無効' do
+      user = build(:user, password: 'test')
+      user.valid?
+      expect(user.errors[:password]).to include('は6文字以上で入力してください')
+    end
+  
+    it 'emailがすでにあるとき無効' do
+      create(:user, email: 'test@example.com')
+      user = build(:user, email: 'test@example.com')
+      user.valid?
+      expect(user.errors[:email]).to include('はすでに存在します')
+    end
+  end
 end
