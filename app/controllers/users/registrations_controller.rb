abf3616d42eccf1768e394355a17fe8d0113b3de
nanairo3 <nao23333@gmail.com>
2019-09-16 23:04:50 +0900

    インデント調整

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -39,7 +39,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   protected
-  
+
     def update_resource(resource, params)
       return super if params['password']&.present?
       resource.update_without_password(params.except('current_password'))
