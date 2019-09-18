005700e7e8c74afde8dced74a7841920689b2a61
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-09-18 00:33:07 +0000

    route削除

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,6 @@ Rails.application.routes.draw do
   
   resources :users, only: [:index, :show]
 
-  devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy"
-  end
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
