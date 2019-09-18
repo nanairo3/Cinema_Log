9bfd902407d161f1abd830a743501e62a267b4ef
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-09-18 01:33:16 +0000

    FactoryBot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,23 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { 'userA' }
+    sequence(:name) { |n| "user{n}" }
     sequence(:email) { |n| "a#{n}@example.com" }
-    password { 'apassword' }
-    password_confirmation { 'apassword' }
-  end
-  
-  factory :test_user, class: 'User' do
-    name { 'test_user' }
-    email { 'test@example.com' }
-    password { 'testpass' }
-    password_confirmation { 'testpass' }
-  end
-  
-  factory :other_user, class: 'User' do
-    sequence(:name) { |n| "otherユーザー_#{n}" }
-    sequence(:email) { |n| "other_#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
   end
-  
 end
