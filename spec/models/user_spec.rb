dceaf7e0fb46e80ea59c45c1453bced9ccac53e1
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-09-19 00:33:12 +0000

    modelテスト修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,35 +9,59 @@ RSpec.describe User, type: :model do
   end
   
    describe '異常系' do
-    before { user.valid? create(:user, email: 'test@example.com') }
     context 'emailがnilの場合' do
       let(:user) { build :user, email: nil }
-      it { expect(user.errors[:email]).to include('を入力してください') }
+
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:email]).to include('を入力してください')
+      end
     end
   
-    context  'nameがnilのとき場合' do
+    context 'nameがnilの場合' do
       let(:user) { build :user, name: nil }
-      it { expect(user.errors[:name]).to include('を入力してください') }
+
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:name]).to include('を入力してください')
+      end
     end
   
-    context  'passwordがnilのとき場合' do
+    context 'passwordがnilの場合' do
       let(:user) { build :user, password: nil }
-      it { expect(user.errors[:password]).to include('を入力してください') }
+
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:password]).to include('を入力してください')
+      end
     end
     
-    context  'nameが20文字以上の時場合' do
+    context 'nameが20文字以上の場合' do
       let(:user) { build(:user, name: "a"*21) }
-      it { expect(user.errors[:name]).to include('は20文字以内で入力してください') }
+
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:name]).to include('は20文字以内で入力してください')
+      end
     end
   
-    context  'passwordが6文字以下で場合' do
-      let(:user){ build(:user, password: 'test') }
-      it { expect(user.errors[:password]).to include('は6文字以上で入力してください') }
+    context 'passwordが6文字以下の場合' do
+      let(:user) { build(:user, password: 'test') }
+
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:password]).to include('は6文字以上で入力してください')
+      end
     end
   
-    context  'emailがすでにあるとき場合' do
-      let(:user) {build(:user, email: 'test@example.com')}
-      it { expect(user.errors[:email]).to include('はすでに存在します') }
+    context 'emailがすでにある場合' do
+      before { create(:user, email: 'test@example.com') }
+      let(:user) { build(:user, email: 'test@example.com') }
+
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:email]).to include('はすでに存在します')
+      end
     end
   end
 end
