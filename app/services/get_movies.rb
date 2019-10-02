cf4ff6953396d9315c4b2a000242c22d2434aa67
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-02 04:28:35 +0000

    now_playing 取得できるもの全てDB保存

--- app/services/get_movies.rb
+++ app/services/get_movies.rb
@@ -7,29 +7,33 @@ class GetMovies
     language = "ja-JP"
     page = 1
     region = "JP"
+    now_playing = ["start set"]
     
-    ENV["SSL_CERT_PATH"] = "/etc/pki/tls"
+    #ENV["SSL_CERT_PATH"] = "/etc/pki/tls"
     connection = Faraday.new("https://api.themoviedb.org") 
     
-    response = connection.get "/3/movie/now_playing" do |request|
-     request.params[:api_key] =  api_key
-     request.params[:language] = language
-     request.params[:page] = page
-     request.params[:region] = region
-    end
-    
-    if response.success?
-      now_playing = JSON.parse(response.body)['results']
-      now_playing.each do |now|
-        if Movie.find_by_id(now['id']).nil? && now['poster_path']
-          Movie.create(id: now['id'], title: now['title'], original_title: now['original_title'],
-                       poster_path: now['poster_path'], popularity: now['popularity'], release_date: now['release_date'],
-                       overview: now['overview'])
-        end
+    while now_playing != []
+      response = connection.get "/3/movie/now_playing" do |request|
+       request.params[:api_key] =  api_key
+       request.params[:language] = language
+       request.params[:page] = page
+       request.params[:region] = region
       end
-    else
-      puts "通信エラー： #{response.status}"
+      
+      if response.success?
+        now_playing = JSON.parse(response.body)['results']
+        now_playing.each do |now|
+          if Movie.find_by_id(now['id']).nil? && now['poster_path']
+            Movie.create(id: now['id'], title: now['title'], original_title: now['original_title'],
+                         poster_path: now['poster_path'], popularity: now['popularity'], release_date: now['release_date'],
+                         overview: now['overview'])
+          end
+        end
+      else
+        puts "通信エラー: #{response.status}"
+      end
+      
+      page += 1
     end
-    
   end
 end
