07898af7ba2b249b7c11c020ae945ad19c235656
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-02 05:37:28 +0000

    movie, genre, movie_detail,video 取得

--- app/services/get_movies.rb
+++ app/services/get_movies.rb
@@ -35,5 +35,43 @@ class GetMovies
       
       page += 1
     end
+    
+    response = connection.get "/3/genre/movie/list" do |request|
+      request.params[:api_key] =  api_key
+      request.params[:language] = language
+    end
+    genre_json = JSON.parse(response.body)["genres"]
+    genre_json.each do |genre|
+      if response.success?
+        if Genre.find_by_id(genre['id']).nil?
+          Genre.create(id: genre['id'], name: genre['name'])
+        end
+      else
+        puts "通信エラー: #{response.status}"
+      end
+    end
+    
+    
+    movies = Movie.all
+    movies.each do |movie|
+      response = connection.get "/3/movie/#{movie.id}" do |request|
+        request.params[:api_key] =  api_key
+        request.params[:language] = language
+        request.params[:append_to_response] = "videos"
+      end
+      
+      if response.success?
+        detail = JSON.parse(response.body)
+        puts detail['videos']['results']
+        if movie.run_time.nil? || movie.video_key.nil?
+          movie.run_time = detail['runtime']
+          movie.video_key = detail['videos']['results']["key"] if detail['videos']['results'] != []
+          movie.save
+        end
+      else
+        puts "通信エラー: #{response.status}"
+      end
+    end
   end
+
 end
