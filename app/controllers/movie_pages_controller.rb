3b92bf1ea6ce53b82768b25e73c391d4da022891
nanairo3 <nao23333@gmail.com>
2019-10-06 21:03:07 +0900

    ページネーション 修正

--- app/controllers/movie_pages_controller.rb
+++ app/controllers/movie_pages_controller.rb
@@ -1,6 +1,6 @@
 class MoviePagesController < ApplicationController
   def home
-    @movies = Movie.all.order(popularity: "DESC").page(params[:page])
+    @movies = Movie.all.order(popularity: "DESC").page(params[:page]).per(12)
   end
   
   def show
