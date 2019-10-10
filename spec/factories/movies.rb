345826dbb2168b78db0c47d8b571bf56d096355c
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-10 09:32:31 +0000

    api test 途中

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,12 +1,16 @@
 FactoryBot.define do
   factory :movie do
-    title { "MyString" }
-    original_title { "MyString" }
-    poster_path { "MyString" }
-    popularity { 1 }
-    release_date { "MyString" }
-    overview { "MyText" }
-    video_key { "MyString" }
-    run_time { 1 }
+    json {
+      {
+        id: 111111,
+        title: "title",
+        original_title: "original_title",
+        poster_path: "poster_path",
+        popularity: 1,
+        release_date: "release_date",
+        overview: "overview",
+        video_: "video_key",
+      }
+    }
   end
 end
--- spec/services/get_movies_spec.rb
+++ spec/services/get_movies_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
-describe GetMovies do
+describe '映画情報取得API' do
   describe '正常系' do
+    before do
+      get_movie_mock = double('Get Movie')
+      expect(get_movie_mock).to receive(:get_movie_json, :get_movie_save)
+      
+      get_movie = GetMovies
+      allow(get_movie).to receive(:get_movie_json, :get_movie_save).and_return(get_movie_mock)
+    end
     
+    content 'get_movie_saveにjsonを渡した場合'do
+      it ''
+    end
   end
 end
