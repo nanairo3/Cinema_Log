532f0c18402cafb05ba70c219f270a39cb73bf70
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-15 00:38:23 +0000

    private対応

--- app/services/get_movies.rb
+++ app/services/get_movies.rb
@@ -6,55 +6,59 @@ class GetMovies
   LANGUAGE = "ja-JP"
   PAGE = 1
   REGION = "JP"
-  CONNECTION = Faraday.new("https://api.themoviedb.org") 
-
-  def self.get_movie_json(get_mode)
-    response = CONNECTION.get "/3/movie/#{get_mode}" do |request|
-      request.params[:api_key] = API_KEY
-      request.params[:language] = LANGUAGE
-      request.params[:page] = PAGE
-      request.params[:region] = REGION
+  CONNECTION = Faraday.new("https://api.themoviedb.org")
+  
+  class << self
+    def api_execution
+      json = get_movie_json("now_playing")
+      json += get_movie_json("upcoming")
+      json.each do |json|
+        get_movie_save(json)
+      end
     end
     
-    raise "通信エラー： #{response.status}" unless response.success? 
-    count = JSON.parse(response.body)['total_pages']
-    result = JSON.parse(response.body)['results']
-    renge = 2..count
-    
-    renge.each do |page|
+    private
+  
+    def get_movie_json(get_mode)
       response = CONNECTION.get "/3/movie/#{get_mode}" do |request|
         request.params[:api_key] = API_KEY
         request.params[:language] = LANGUAGE
-        request.params[:page] =  page
+        request.params[:page] = PAGE
         request.params[:region] = REGION
       end
       
-      raise "通信エラー: #{response.status}" unless response.success?
-      result.push(JSON.parse(response.body)['results'])
+      raise "通信エラー： #{response.status}" unless response.success? 
+      count = JSON.parse(response.body)['total_pages']
+      result = JSON.parse(response.body)['results']
+      renge = 2..count
+      
+      renge.each do |page|
+        response = CONNECTION.get "/3/movie/#{get_mode}" do |request|
+          request.params[:api_key] = API_KEY
+          request.params[:language] = LANGUAGE
+          request.params[:page] =  page
+          request.params[:region] = REGION
+        end
+        
+        raise "通信エラー: #{response.status}" unless response.success?
+        result.push(JSON.parse(response.body)['results'])
+      end
+      
+      result.flatten
     end
     
-    result.flatten
-  end
-  
-  def self.get_movie_save(json)
-    movie = Movie.find_or_initialize_by(id: json["id"])
-    movie.assign_attributes(
-      title: json["title"] || "",
-      original_title: json["original_title"] || "",
-      poster_path: json["poster_path"] || "",
-      popularity: json["popularity"],
-      release_date: json["release_date"],
-      overview: json["overview"],
-    )
-    movie.save!
-  end
-  
-  def self.api_execution
-    json = get_movie_json("now_playing")
-    json += get_movie_json("upcoming")
-    json.each do |json|
-      get_movie_save(json)
+    def get_movie_save(json)
+      movie = Movie.find_or_initialize_by(id: json["id"])
+      movie.assign_attributes(
+        title: json["title"] || "",
+        original_title: json["original_title"] || "",
+        poster_path: json["poster_path"] || "",
+        popularity: json["popularity"],
+        release_date: json["release_date"],
+        overview: json["overview"],
+      )
+      movie.save!
     end
+  
   end
-    
 end
