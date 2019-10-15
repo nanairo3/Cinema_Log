0311b87471a804babb2fe471f44633a873792783
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-15 02:20:03 +0000

    APIテスト作成

--- spec/services/get_movies_spec.rb
+++ spec/services/get_movies_spec.rb
@@ -1,17 +1,70 @@
 require 'rails_helper'
 
+FactoryBot.define do
+  factory :movie do
+    [{
+      "id" => 111111,
+      "title" =>"movie",
+      "original_title" => "movie",
+      "poster_path" => "poster_path",
+      "popularity" => 1,
+      "release_date" => "release_date",
+      "overview" => "overview",
+      "video_key" => "video_key"
+    }]
+  end
+end
+
+
 describe '映画情報取得API' do
-  describe '正常系' do
-    let!(:json){ create(:movie) }
+  describe '.api_execution' do
+    let!(:exist_movie) { create :movie, id: 111111 }
     
     before do
-      allow(GetMovies).to receive(:get_movie_save).and_return('get movie save mock')
+      allow(GetMovies).to receive(:get_movie_json) do |get_mode|
+        if get_mode == 'now_playing'
+          [{
+            "id" => 111111,
+            "title" =>"now_playing",
+            "original_title" => "now_playing",
+            "poster_path" => "poster_path",
+            "popularity" => 1,
+            "release_date" => "release_date",
+            "overview" => "overview",
+            "video_key" => "video_key"
+          }]
+        elsif get_mode == 'upcoming'
+          [{
+            "id" => 222222,
+            "title" =>"upcoming",
+            "original_title" => "upcoming",
+            "poster_path" => "poster_path",
+            "popularity" => 1,
+            "release_date" => "release_date",
+            "overview" => "overview",
+            "video_key" => "video_key"
+          }]
+        end
+      end
+      expect(Movie.count).to eq 1
     end
     
-    context 'get_movie_saveにjsonを渡した場合'do
-      it 'エラーなく保存できる' do
-        expect{ GetMovies.get_movie_save(json) }.not_to raise_error
-      end
+    it '映画数が全体で2件となっていること' do
+      GetMovies.api_execution
+      expect(Movie.count).to eq 2
+    end
+
+    it 'APIで追加したデータが、正しく保存されていること' do
+      GetMovies.api_execution
+      expect(Movie.find(111111).title).to eq 'now_playing'
+      expect(Movie.find(222222).title).to eq 'upcoming'
+    end
+
+    it '既存の映画レコードが更新されていること' do
+      old = Movie.find(111111)
+      GetMovies.api_execution
+      new = Movie.find(111111)
+      expect(new.title).not_to eq old.title
     end
     
   end
