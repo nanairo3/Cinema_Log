405ff37737f4234a51c68de96cafc67c70b0fa46
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-16 02:09:20 +0000

    FactoryBot修正

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,13 +1,11 @@
 FactoryBot.define do
   factory :movie do
-    [{
-      "id" => 111111,
-      "title" =>"movie",
-      "original_title" => "movie",
-      "poster_path" => "poster_path",
-      "popularity" => 1,
-      "release_date" => "release_date",
-      "overview" => "overview"
-    }]
+      id  { 111111 }
+      sequence(:title) { |n| "#{n}movie"}
+      sequence(:original_title) { |n| "#{n}movie"}
+      poster_path { "poster_path" }
+      popularity { 1 }
+      release_date { "release_date" }
+      overview { "overview" }
   end
 end
