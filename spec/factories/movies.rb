983b02a2e4986fc889b2fea7d11ff50fefe8f020
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-18 02:23:37 +0000

    インデント調整

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :movie do
-      id  { 111111 }
-      sequence(:title) { |n| "#{n}movie"}
-      sequence(:original_title) { |n| "#{n}movie"}
-      poster_path { "poster_path" }
-      popularity { 1 }
-      release_date { "2019-10-17" }
-      overview { "overview" }
+    id  { 111111 }
+    sequence(:title) { |n| "#{n}movie"}
+    sequence(:original_title) { |n| "#{n}movie"}
+    poster_path { "poster_path" }
+    popularity { 1 }
+    release_date { "2019-10-17" }
+    overview { "overview" }
   end
 end
