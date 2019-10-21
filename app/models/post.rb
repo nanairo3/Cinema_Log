5dc9c7bce2582b120fd5929e67caf2610461c29a
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-21 00:37:07 +0000

    post DB,model作成

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  belongs_to :movie
+  
+  validates :content, presence: true, length: {maximum: 500}
+end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :post do
+    content { "MyText" }
+    user { "" }
+    movie { "" }
+  end
+end
