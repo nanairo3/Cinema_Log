442083a5c8007e088e4f25717c443ac649783eaf
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-23 00:31:34 +0000

    hashスペース修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :movie
   
-  validates :content, presence: true, length: {maximum: 500}
+  validates :content, presence: true, length: { maximum: 500 }
 end
