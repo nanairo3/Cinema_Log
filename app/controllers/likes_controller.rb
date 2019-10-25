6d75534bdaf8968e20ed6b698ec694ae2a739626
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-25 01:52:19 +0000

    like controller作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,15 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    like = current_user.likes.create(post_id: params[:id])
+    redirect_back(fallback_location: root_path)
+  end
+  
+  def destroy
+    like = current_user.likes.find_by(post_id: params[:id])
+    like.destroy
+    redirect_back(fallback_location: root_path)
+  end
+  
 end
