4a64d6f4951ff4776fe69ef971cf4b4a9c2ef23f
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-29 01:31:41 +0000

    movie idをシーケンスに変更

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :movie do
-    id  { 111111 }
+    sequence(:id)  { |n| 11111 + n }
     sequence(:title) { |n| "#{n}movie"}
     sequence(:original_title) { |n| "#{n}movie"}
     poster_path { "poster_path" }
