513598f404611fe10dea39ae1b01f6282d06e14e
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-29 01:15:32 +0000

    modelテスト作成

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user){ create(:user) }
+  let!(:post){ create(:post) }
+  
+  describe '正常系' do
+    context '正しくいいねできる場合' do
+      let(:like){ build(:like, user: user, post: post) }
+      it { expect(like).to be_valid }
+    end
+  end
 end
