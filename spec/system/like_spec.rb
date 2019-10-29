6f4e9c246c0a04f3c27527eeebc5aa236e00b168
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-10-29 01:47:34 +0000

    systemテスト完成

---
+++ spec/system/like_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'いいね機能', type: :system do
+  let!(:user){ create(:user) }
+  let!(:movie){ create(:movie) }
+  let!(:post){ create(:post, movie: movie) }
+
+  before do
+    sign_in_as user
+  end
+  
+  describe '正常系' do
+    describe '映画詳細画面' do
+      before do
+        visit movie_path(movie)
+      end
+      
+      context 'いいねをクリックした場合' do
+        it 'いいねできる' do
+          find('.far').click
+          expect(page).to have_css '.fas'
+          expect(page).to have_css "div#like-post-#{post.id}", text: '1'
+        end
+      end
+      
+      context 'いいねを削除した場合' do
+        it 'いいねを取り消せる' do
+          find('.far').click
+          find('.fas').click
+          expect(page).to have_css '.far'
+          expect(page).to have_css "div#like-post-#{post.id}", text: '0'
+        end
+      end
+    end
+  end
+end
