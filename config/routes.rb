18c453b6397424454e3c0a615efbdde9623d21a9
nanairo3 <nao23333@gmail.com>
2019-10-30 14:33:51 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,18 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-  
+
   resources :movies, only: [:index, :show]
   resources :users, only: [:index, :show]
-  
+
   resources :posts, only: [:show, :destroy, :edit, :update]
   resources :movies, only: [:show] do
     resources :posts, only: [:new, :create]
   end
-  
-  resources :posts, only: [:show] do
-    resources :likes, only: [:create]
+
+  resources :posts do
+    resource :likes, only: [:create, :destroy]
   end
-  delete '/posts/:post_id/likes' => 'likes#destroy'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
