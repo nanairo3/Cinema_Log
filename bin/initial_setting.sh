9185c2964e5e6cce048e19264dd12ca12fd7021c
nanairo3 <ec2-user@ip-172-31-45-164.ap-northeast-1.compute.internal>
2019-11-08 07:38:03 +0000

    初期設定コマンドのディレクトリ移動

---
+++ bin/initial_setting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+rm -f /my_app/tmp/pids/server.pid
+
+bundle exec rake db:create
+bundle exec rake db:migrate
+rails runner MoviesAcquisitionApiSercvice.import
+bundle exec rails s -p 3000 -b '0.0.0.0'
+
+exec "$@"
