544709bfccd815ae7b7e228b77980ad6b0659e56
nanairo3 <nao23333@gmail.com>
2019-11-13 23:17:55 +0900

    rubocop -a --only Layout/IndentationConsistency

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,50 +20,50 @@ RSpec.describe User, type: :model do
       end
     end
 
-   context 'nameがnilの場合' do
-     let(:user) { build :user, name: nil }
+    context 'nameがnilの場合' do
+      let(:user) { build :user, name: nil }
 
-     it 'ユーザー登録できない' do
-       user.valid?
-       expect(user.errors[:name]).to include('を入力してください')
-     end
-   end
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:name]).to include('を入力してください')
+      end
+    end
 
-   context 'passwordがnilの場合' do
-     let(:user) { build :user, password: nil }
+    context 'passwordがnilの場合' do
+      let(:user) { build :user, password: nil }
 
-     it 'ユーザー登録できない' do
-       user.valid?
-       expect(user.errors[:password]).to include('を入力してください')
-     end
-   end
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:password]).to include('を入力してください')
+      end
+    end
 
-   context 'nameが20文字以上の場合' do
-     let(:user) { build(:user, name: 'a' * 21) }
+    context 'nameが20文字以上の場合' do
+      let(:user) { build(:user, name: 'a' * 21) }
 
-     it 'ユーザー登録できない' do
-       user.valid?
-       expect(user.errors[:name]).to include('は20文字以内で入力してください')
-     end
-   end
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:name]).to include('は20文字以内で入力してください')
+      end
+    end
 
-   context 'passwordが6文字以下の場合' do
-     let(:user) { build(:user, password: 'test') }
+    context 'passwordが6文字以下の場合' do
+      let(:user) { build(:user, password: 'test') }
 
-     it 'ユーザー登録できない' do
-       user.valid?
-       expect(user.errors[:password]).to include('は6文字以上で入力してください')
-     end
-   end
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:password]).to include('は6文字以上で入力してください')
+      end
+    end
 
-   context 'emailがすでにある場合' do
-     before { create(:user, email: 'test@example.com') }
-     let(:user) { build(:user, email: 'test@example.com') }
+    context 'emailがすでにある場合' do
+      before { create(:user, email: 'test@example.com') }
+      let(:user) { build(:user, email: 'test@example.com') }
 
-     it 'ユーザー登録できない' do
-       user.valid?
-       expect(user.errors[:email]).to include('はすでに存在します')
-     end
-   end
+      it 'ユーザー登録できない' do
+        user.valid?
+        expect(user.errors[:email]).to include('はすでに存在します')
+      end
+    end
   end
 end
--- spec/system/user_login_spec.rb
+++ spec/system/user_login_spec.rb
@@ -4,38 +4,38 @@ require 'rails_helper'
 
 describe 'ログイン機能', type: :system do
   let!(:user) { create(:user) }
-    let!(:test_user) { create(:user) }
+  let!(:test_user) { create(:user) }
 
-    before do
-      visit new_user_session_path
+  before do
+    visit new_user_session_path
+  end
+
+  describe '正常系' do
+    context '正しくemail、passwordを入力した場合' do
+      it 'ログインできる' do
+        fill_in 'session_email', with: user.email
+        fill_in 'session_password', with: user.password
+        click_button 'Login'
+        expect(page).to have_content 'ログインしました。'
+      end
     end
+  end
 
-    describe '正常系' do
-      context '正しくemail、passwordを入力した場合' do
-        it 'ログインできる' do
-          fill_in 'session_email', with: user.email
-          fill_in 'session_password', with: user.password
-          click_button 'Login'
-          expect(page).to have_content 'ログインしました。'
-        end
+  describe '異常系' do
+    context '間違ったpasswordを入力した場合' do
+      it 'ログインできない' do
+        fill_in 'session_email', with: user.email
+        fill_in 'session_password', with: 'nonpassword'
+        click_button 'Login'
+        expect(page).to have_content 'Eメールまたはパスワードが違います。'
       end
     end
 
-    describe '異常系' do
-      context '間違ったpasswordを入力した場合' do
-        it 'ログインできない' do
-          fill_in 'session_email', with: user.email
-          fill_in 'session_password', with: 'nonpassword'
-          click_button 'Login'
-          expect(page).to have_content 'Eメールまたはパスワードが違います。'
-        end
+    context '未入力の場合' do
+      it 'should have_content Eメールまたはパスワードが違います' do
+        click_button 'Login'
+        expect(page).to have_content 'Eメールまたはパスワードが違います。'
       end
-
-        context '未入力の場合' do
-          it 'should have_content Eメールまたはパスワードが違います' do
-            click_button 'Login'
-            expect(page).to have_content 'Eメールまたはパスワードが違います。'
-          end
-        end
     end
+  end
 end
