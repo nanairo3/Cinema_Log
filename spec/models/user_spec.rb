609b50a528d54438cbbd885e7710f66293524b11
nanairo3 <nao23333@gmail.com>
2019-11-13 22:55:40 +0900

    rubocop -a --only Layout/IndentationConsistency

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,60 +10,60 @@ RSpec.describe User, type: :model do
     end
   end
 
-   describe '異常系' do
-    context 'emailがnilの場合' do
-      let(:user) { build :user, email: nil }
+  describe '異常系' do
+   context 'emailがnilの場合' do
+     let(:user) { build :user, email: nil }
 
-      it 'ユーザー登録できない' do
-        user.valid?
-        expect(user.errors[:email]).to include('を入力してください')
-      end
-    end
+     it 'ユーザー登録できない' do
+       user.valid?
+       expect(user.errors[:email]).to include('を入力してください')
+     end
+   end
 
-    context 'nameがnilの場合' do
-      let(:user) { build :user, name: nil }
+   context 'nameがnilの場合' do
+     let(:user) { build :user, name: nil }
 
-      it 'ユーザー登録できない' do
-        user.valid?
-        expect(user.errors[:name]).to include('を入力してください')
-      end
-    end
+     it 'ユーザー登録できない' do
+       user.valid?
+       expect(user.errors[:name]).to include('を入力してください')
+     end
+   end
 
-    context 'passwordがnilの場合' do
-      let(:user) { build :user, password: nil }
+   context 'passwordがnilの場合' do
+     let(:user) { build :user, password: nil }
 
-      it 'ユーザー登録できない' do
-        user.valid?
-        expect(user.errors[:password]).to include('を入力してください')
-      end
-    end
+     it 'ユーザー登録できない' do
+       user.valid?
+       expect(user.errors[:password]).to include('を入力してください')
+     end
+   end
 
-    context 'nameが20文字以上の場合' do
-      let(:user) { build(:user, name: 'a'*21) }
+   context 'nameが20文字以上の場合' do
+     let(:user) { build(:user, name: 'a'*21) }
 
-      it 'ユーザー登録できない' do
-        user.valid?
-        expect(user.errors[:name]).to include('は20文字以内で入力してください')
-      end
-    end
+     it 'ユーザー登録できない' do
+       user.valid?
+       expect(user.errors[:name]).to include('は20文字以内で入力してください')
+     end
+   end
 
-    context 'passwordが6文字以下の場合' do
-      let(:user) { build(:user, password: 'test') }
+   context 'passwordが6文字以下の場合' do
+     let(:user) { build(:user, password: 'test') }
 
-      it 'ユーザー登録できない' do
-        user.valid?
-        expect(user.errors[:password]).to include('は6文字以上で入力してください')
-      end
-    end
+     it 'ユーザー登録できない' do
+       user.valid?
+       expect(user.errors[:password]).to include('は6文字以上で入力してください')
+     end
+   end
 
-    context 'emailがすでにある場合' do
-      before { create(:user, email: 'test@example.com') }
-      let(:user) { build(:user, email: 'test@example.com') }
+   context 'emailがすでにある場合' do
+     before { create(:user, email: 'test@example.com') }
+     let(:user) { build(:user, email: 'test@example.com') }
 
-      it 'ユーザー登録できない' do
-        user.valid?
-        expect(user.errors[:email]).to include('はすでに存在します')
-      end
-    end
-  end
+     it 'ユーザー登録できない' do
+       user.valid?
+       expect(user.errors[:email]).to include('はすでに存在します')
+     end
+   end
+ end
 end
--- spec/system/user_edit_spec.rb
+++ spec/system/user_edit_spec.rb
@@ -5,71 +5,71 @@ require 'rails_helper'
 describe 'ユーザ編集機能', type: :system do
     let!(:user) { create(:user) }
 
-  before do
-    sign_in_as user
-    visit edit_user_registration_path
-  end
-
-  describe '正常系' do
-    context '正しいimage,name,email,current_password.passwordを入力した場合' do
-      it '正常にアカウント情報を変更される' do
-        file_path = Rails.root.join('spec', 'images', 'test_sample.png')
-        attach_file 'user_image', file_path
-        fill_in 'user_name', with: 'Taro'
-        fill_in 'user_email', with: 'taro@example.com'
-        fill_in 'user_current_password', with: user.password
-        fill_in 'user_password', with: 'taropassword'
-        fill_in 'user_password_confirmation', with: 'taropassword'
-        click_button '更新'
-        expect(page).to have_content 'アカウント情報を変更しました'
-      end
+    before do
+      sign_in_as user
+      visit edit_user_registration_path
     end
-  end
 
-  describe '異常系' do
-      context 'name未入力の場合' do
-          it 'アカウント情報を変更できない' do
-            fill_in 'user_name', with: ''
-            click_button '更新'
-            expect(page).to have_content 'Nameを入力してください'
-          end
-  
-        context 'email未入力の場合' do
-          it 'アカウント情報を変更できない' do
-            fill_in 'user_email', with: ''
-            click_button '更新'
-            expect(page).to have_content 'Eメールを入力してください'
-          end
-        end
-  
-        context '現在のパスワードが間違っている場合'
-          it 'アカウント情報を変更できない' do
-            fill_in 'user_current_password', with: '間違いpassword'
-            fill_in 'user_password', with: 'taropassword'
-            fill_in 'user_password_confirmation', with: 'taropassword'
-            click_button '更新'
-            expect(page).to have_content '現在のパスワードは不正な値です'
-          end
-        end
-
-      context '新しいパスワードと確認が間違っている場合' do
-        it 'アカウント情報を変更できない' do
-          fill_in 'user_current_password', with: 'apassword'
+    describe '正常系' do
+      context '正しいimage,name,email,current_password.passwordを入力した場合' do
+        it '正常にアカウント情報を変更される' do
+          file_path = Rails.root.join('spec', 'images', 'test_sample.png')
+          attach_file 'user_image', file_path
+          fill_in 'user_name', with: 'Taro'
+          fill_in 'user_email', with: 'taro@example.com'
+          fill_in 'user_current_password', with: user.password
           fill_in 'user_password', with: 'taropassword'
-          fill_in 'user_password_confirmation', with: '間違いpassword'
+          fill_in 'user_password_confirmation', with: 'taropassword'
           click_button '更新'
-          expect(page).to have_content 'パスワード（確認用）とパスワードの入力が一致しません'
-        end
-      end
-
-      context '新しいパスワードが6文字未満の場合' do
-        it 'アカウント情報を変更できない' do
-          fill_in 'user_current_password', with: 'apassword'
-          fill_in 'user_password', with: '12345'
-          fill_in 'user_password_confirmation', with: '12345'
-          click_button '更新'
-          expect(page).to have_content 'パスワードは6文字以上で入力してください'
+          expect(page).to have_content 'アカウント情報を変更しました'
         end
       end
     end
+
+    describe '異常系' do
+        context 'name未入力の場合' do
+            it 'アカウント情報を変更できない' do
+              fill_in 'user_name', with: ''
+              click_button '更新'
+              expect(page).to have_content 'Nameを入力してください'
+            end
+    
+            context 'email未入力の場合' do
+              it 'アカウント情報を変更できない' do
+                fill_in 'user_email', with: ''
+                click_button '更新'
+                expect(page).to have_content 'Eメールを入力してください'
+              end
+            end
+    
+            context '現在のパスワードが間違っている場合'
+            it 'アカウント情報を変更できない' do
+              fill_in 'user_current_password', with: '間違いpassword'
+              fill_in 'user_password', with: 'taropassword'
+              fill_in 'user_password_confirmation', with: 'taropassword'
+              click_button '更新'
+              expect(page).to have_content '現在のパスワードは不正な値です'
+            end
+          end
+  
+        context '新しいパスワードと確認が間違っている場合' do
+          it 'アカウント情報を変更できない' do
+            fill_in 'user_current_password', with: 'apassword'
+            fill_in 'user_password', with: 'taropassword'
+            fill_in 'user_password_confirmation', with: '間違いpassword'
+            click_button '更新'
+            expect(page).to have_content 'パスワード（確認用）とパスワードの入力が一致しません'
+          end
+        end
+  
+        context '新しいパスワードが6文字未満の場合' do
+          it 'アカウント情報を変更できない' do
+            fill_in 'user_current_password', with: 'apassword'
+            fill_in 'user_password', with: '12345'
+            fill_in 'user_password_confirmation', with: '12345'
+            click_button '更新'
+            expect(page).to have_content 'パスワードは6文字以上で入力してください'
+          end
+        end
+      end
   end
