4b6018f8a4a424523defe105b870c9c887bf1cca
nanairo3 <nao23333@gmail.com>
2019-11-13 22:34:27 +0900

    rubocop -a --only Layout/SpaceInsideBlockBraces

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :movie do
     sequence(:id)  { |n| 11111 + n }
-    sequence(:title) { |n| "#{n}movie"}
-    sequence(:original_title) { |n| "#{n}movie"}
+    sequence(:title) { |n| "#{n}movie" }
+    sequence(:original_title) { |n| "#{n}movie" }
     poster_path { "poster_path" }
     popularity { 1 }
     release_date { "2019-10-17" }
