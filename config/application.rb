818198f344a0156dba29ae4ac716178842d19f89
nanairo3 <nao23333@gmail.com>
2019-11-13 23:06:21 +0900

    rubocop -a --only Layout/AlignArguments

--- config/application.rb
+++ config/application.rb
@@ -37,10 +37,10 @@ module Environment
       g.stylesheets     false
       g.helper          false
       g.test_framework :rspec,
-         fixtures: false,
-         view_specs: false,
-         helper_specs: false,
-         routing_specs: false
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
