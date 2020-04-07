757d16cf510b4df9f9ca67c49909d8965a7a88d3
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-04-07 14:56:59 +0900

    add ルート、トップ画面に検索フォームを追加

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
     resource :like, only: %i[create destroy]
   end
 
+  resources :search, only: %i[index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
