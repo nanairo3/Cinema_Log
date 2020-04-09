de858b8756d1bf2c11e53189a9201d5fb90435a9
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-04-09 14:35:34 +0900

    systemテスト追加

--- spec/system/search_spec.rb
+++ spec/system/search_spec.rb
@@ -16,7 +16,24 @@ describe 'serach', type: :system, js: true do
       expect(page).to have_content "原題：Sonic the Hedgehog"
     end
     context "検索ワードを入力しない場合" do
-      it '検索ワードを入力してくださいと'
+      it '現在のページにリダイレクトされる' do
+        fill_in "keyword",	with: " "
+        expect(page).to have_content "注目の映画"
+        expect(page).to_not have_content "検索結果"
+
+        fill_in "keyword",	with: "ソニック"
+        find('#icon_prefix2').native.send_key(:enter)
+        expect(page).to_not have_content "注目の映画"
+        expect(page).to have_content "検索結果"
+      end
+    end
+    
+    context "検索結果がない場合" do
+      it '検索結果ページに、検索結果がないと表示される' do
+        fill_in "keyword",	with: "あああああ"
+        find('#icon_prefix2').native.send_key(:enter)
+        expect(page).to have_content "検索結果がありません"
+      end
     end
     
   end
